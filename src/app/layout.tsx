d78ff3e57afddb60ed3072d19d2f722414dd471c
RevainIDN <kov.igor94@gmail.com>
2026-02-21 17:50:52 +0200

    refactor: update root layout and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import { Inter } from "next/font/google";
 import '../styles/globals.css';
 
-import Header from "@/components/layout/Header/Header";
-import Footer from "@/components/layout/Footer/Footer";
-
 const inter = Inter({
   subsets: ["latin"],
   weight: ["300", "400", "700"],
@@ -18,9 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
